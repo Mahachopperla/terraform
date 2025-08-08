7d8890316f98f6b312fb337230042da552aecca1
maha <maridayya@gmail.com>
2025-08-08 19:17:48 +0530

    helm-installation

--- docker_instance/docker_installation.sh
+++ docker_instance/docker_installation.sh
@@ -40,3 +40,9 @@ sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+
+#installing helm
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
